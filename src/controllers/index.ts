74ac6c866088dc63f761ceeefbcc5c72fe3e5d9f
Francesco Petru Andrei <535058@student.fontys.nl>
2024-10-07 14:52:19 +0200

    xdrtcfyvgbhjk

---
+++ src/controllers/index.ts
@@ -0,0 +1,5 @@
+export * from './ping.controller';
+export * from './adress.controller';
+export * from './controller.controller';
+export * from './friend-address.controller';
+export * from './address-friend.controller';
